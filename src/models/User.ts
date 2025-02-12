5cb4a60018bb913745f943db7914a6421aac9567
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-12 12:29:59 +0200

    refactor: user calss divided into separate Attributes class

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,17 +2,13 @@ import type { IUserProps } from '@libs/types';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { BASE_URL } from '@libs/constants';
+import { Attributes } from './Attributes';
 
 export class User {
   public sync: Sync<IUserProps> = new Sync<IUserProps>(BASE_URL, '/users');
+  public attributes: Attributes<IUserProps>;
 
-  constructor(private data: IUserProps, public events: Eventing = new Eventing()) {}
-
-  get(propName: keyof IUserProps): string | number | undefined {
-    return this.data[propName];
-  }
-
-  set(update: IUserProps): void {
-    Object.assign(this.data, update);
+  constructor(attributes: IUserProps, public events: Eventing = new Eventing()) {
+    this.attributes = new Attributes<IUserProps>(attributes);
   }
 }
