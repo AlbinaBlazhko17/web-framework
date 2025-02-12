aa983599604b8bbb2d5388744fb385af1f20602c
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-12 14:45:28 +0200

    refactor: shorten passthrough methods in Model class

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -3,17 +3,9 @@ import type { IAttributes, IEventing, IApiSync, IdObject } from '@libs/types';
 export class Model<T extends IdObject> {
   constructor(private attributes: IAttributes<T>, private events: IEventing, private sync: IApiSync<T>) {}
 
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
