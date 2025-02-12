65d1f6f57fdc94fab1a815e3e8ad189d703cc099
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-12 16:02:38 +0200

    feat: implement binding events functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserForm } from './views';
+import { User } from './models';
 
-const userForm = new UserForm(document.getElementById('root') as Element);
+const user = User.buildUser({ name: 'NAME', age: 20 });
+
+const userForm = new UserForm(document.getElementById('root') as Element, user);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,18 +1,52 @@
+import type { User } from '@/models';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'click:.set-age': this.onSetAgeClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there!');
+  }
+
+  onSetAgeClick = (): void => {
+    console.log('Set random age');
+  };
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User Name:${this.model.get('name')}</div>
+        <div>User Age:${this.model.get('age')}</div>
         <input />
+        <button>Click Me</button>
+        <button class='set-age'>Set random age</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
